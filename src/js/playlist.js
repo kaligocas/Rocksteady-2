ea5f1876fa27e29ac0b7fea6dc496c2377a3bc86
katherine gomez <kaligocas1990@gmail.com>
2023-12-09 23:11:37 +0100

    created page playlist

---
+++ src/js/playlist.js
@@ -0,0 +1,18 @@
+/*document.addEventListener('DOMContentLoaded', function () {
+    const playlistElement = document.getElementById('playlist');
+    const audioPlayer = document.getElementById('audioPlayer');
+  
+    const playlist = [
+      { title: 'Canción 1', src: './assets/Alton Ellis Girl I ve Got A Date-.mp3' },
+      { title: 'Canción 2', src: './assets/Alton Ellis Rocksteady.mp3' },
+
+    ];
+  
+    playlist.forEach((song, index) => {
+      const listItem = document.createElement('li');
+      listItem.textContent = `${index + 1}. ${song.title}`;
+      listItem.classList.add('list-group-item');
+      listItem.addEventListener('click', () => playSong(index));
+      playlistElement.appendChild(listItem);
+    });
+*/

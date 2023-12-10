d2447887d9c2e84faf4935a59f0ea24e30851fdd
katherine gomez <kaligocas1990@gmail.com>
2023-12-11 00:47:47 +0100

    modified js playlist

--- src/js/playlist.js
+++ src/js/playlist.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+/*document.addEventListener('DOMContentLoaded', function () {
   const audioPlayer = document.getElementById('audioPlayer');
   const playButton = document.getElementById('playButton');
   playButton.addEventListener('click', function () {
@@ -10,9 +10,27 @@ document.addEventListener('DOMContentLoaded', function () {
       playButton.textContent = 'Play';
     }
   });
-});
+});*/
+
+
 
 /*document.getElementById('playButton').addEventListener('click', function() {
   var audio = document.getElementById('audioPlayer');
   audio.play();
 });*/
+
+  var audio;
+  var playlist;
+  var tracks;
+  var current;
+  initaudio();
+  function initaudio(){current=0;audio=$('audio');
+  playlist=$('#playlist');
+  tracks=playlist.find('li a');
+  len=tracks.length-1;audio[0].volume=1;playlist.find('a').click(function(e){e.preventDefault();
+  link=$(this);
+  current=link.parent().index();
+  runaudio(link,audio[0])});
+  audio[0].addEventListener('ended',function(e){current++;
+    if(current>len){current=0;link=playlist.find('a')[0]}else{link=playlist.find('a')[current]}runaudio($(link),audio[0])})}function runaudio(link,player){player.src=link.attr('href');par=link.parent();par.addClass('active').siblings().removeClass('active');audio[0].load();audio[0].play()}
+  
